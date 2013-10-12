f1720966192efaeda9aabceb02d51e83a6cf9e5f
nnard1616 <nnard1616@gmail.com>
2013-10-11 20:15:42 -0500

    changing organization

--- daynumber.h
+++
@@ -1,8 +0,0 @@
-#ifndef DAYNUMBER_H
-#define DAYNUMBER_H
-#include <iostream>
-using namespace std;
-
-int dayNumberCalculator(int iMonth, int iDay, int iYear);//Calculates the day number of the year.
-int februaryCalculator(int iYear);//Calculates number of days in february
-#endif // DAYNUMBER_H
